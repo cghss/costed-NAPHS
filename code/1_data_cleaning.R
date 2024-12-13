e9b5ae37ad292c8e7b509b554acd5001dca1da8b
seaneff <steph.eaneff@gmail.com>
2024-12-13 09:01:20 -0800

    more manual data cleaning and checks

--- code/1_data_cleaning.R
+++ code/1_data_cleaning.R
@@ -3,10 +3,15 @@
 ###########################################################################
 
 library(readxl) ## to read in Excel files
+library(dplyr) ## for data manipulation
 
 ###########################################################################
 ## Read in raw data #######################################################
 ###########################################################################
+
+## TODO: check start year of Uganda NAPHS, Q/A raw data again
+## TODO: update data entry guidelines with new year coding rules
+## TODO: check currency for Nigeria
 
 ## get a list of all the countries in the sheet (one per tab)
 all_countries <- excel_sheets("data/raw/Costed NAPHS data.xlsx")
@@ -24,6 +29,17 @@ for(i in 1:length(all_countries)-1){
 ###########################################################################
 ## Restructure and rename variables #######################################
 ###########################################################################
+
+## rename variables
+names(raw_data)[which(names(raw_data) == "Country")] <- "country"
+names(raw_data)[which(names(raw_data) == "Capacity")] <- "capacity"
+names(raw_data)[which(names(raw_data) == "Requirement")] <- "requirement"
+names(raw_data)[which(names(raw_data) == "Action")] <- "action"
+names(raw_data)[which(names(raw_data) == "Year (1-5)")] <- "year_numeric"
+names(raw_data)[which(names(raw_data) == "Year (calendar)")] <- "year_calendar"
+names(raw_data)[which(names(raw_data) == "Cost")] <- "cost"
+names(raw_data)[which(names(raw_data) == "Currency (year)")] <- "currency"
+names(raw_data)[which(names(raw_data) == "Flag")] <- "flag"
 
 ###########################################################################
 ## Export clean dataset ###################################################
