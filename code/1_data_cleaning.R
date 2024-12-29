54e00117db0c389d7fe01168e9d703db04265e22
seaneff <steph.eaneff@gmail.com>
2024-12-29 15:48:43 -0800

    more data cleanup

--- code/1_data_cleaning.R
+++ code/1_data_cleaning.R
@@ -3,26 +3,23 @@
 ###########################################################################
 
 library(readxl) ## to read in Excel files
+library(writexl) ## to save Excel files
 library(dplyr) ## for data manipulation
 
 ###########################################################################
 ## Read in raw data #######################################################
 ###########################################################################
 
-## TODO: check start year of Uganda NAPHS, Q/A raw data again
-## TODO: update data entry guidelines with new year coding rules
-## TODO: check currency for Nigeria
-
 ## get a list of all the countries in the sheet (one per tab)
-all_countries <- excel_sheets("data/raw/Costed NAPHS data.xlsx")
+all_countries <- excel_sheets("data/raw/Raw Costed NAPHS data.xlsx")
 
 ## read in data for all countries and append into a data frame
 ## note, by design last tab is excluded as it's the data dictionary
 for(i in 1:length(all_countries)-1){
-  if (i == 1){ raw_data <- read_excel("data/raw/Costed NAPHS data.xlsx", sheet = all_countries[i]) }
+  if (i == 1){ raw_data <- read_excel("data/raw/Raw Costed NAPHS data.xlsx", sheet = all_countries[i]) }
   if (i >  1){ raw_data <- rbind.data.frame(
                               raw_data,
-                              read_excel("data/raw/Costed NAPHS data.xlsx", sheet = all_countries[i]))
+                              read_excel("data/raw/Raw Costed NAPHS data.xlsx", sheet = all_countries[i]))
                               }
 }
 
@@ -33,14 +30,32 @@ for(i in 1:length(all_countries)-1){
 ## rename variables
 names(raw_data)[which(names(raw_data) == "Country")] <- "country"
 names(raw_data)[which(names(raw_data) == "Capacity")] <- "capacity"
-names(raw_data)[which(names(raw_data) == "Requirement")] <- "requirement"
-names(raw_data)[which(names(raw_data) == "Action")] <- "action"
+names(raw_data)[which(names(raw_data) == "Requirement")] <- "requirement_raw"
+names(raw_data)[which(names(raw_data) == "Action")] <- "action_raw"
 names(raw_data)[which(names(raw_data) == "Year (1-5)")] <- "year_numeric"
 names(raw_data)[which(names(raw_data) == "Year (calendar)")] <- "year_calendar"
-names(raw_data)[which(names(raw_data) == "Cost")] <- "cost"
+names(raw_data)[which(names(raw_data) == "Cost")] <- "cost_raw"
 names(raw_data)[which(names(raw_data) == "Currency (year)")] <- "currency"
 names(raw_data)[which(names(raw_data) == "Flag")] <- "flag"
 
 ###########################################################################
+## Clean written descriptions (minimally) #################################
+###########################################################################
+
+## replace line breaks with spaces in select text fields
+raw_data$requirement <- gsub("\n", " ", raw_data$requirement_raw)
+raw_data$action <- gsub("\n", " ", raw_data$action_raw)
+
+## remove any commas from cost field
+raw_data$cost <- gsub(",", "", raw_data$cost_raw)
+
+###########################################################################
 ## Export clean dataset ###################################################
 ###########################################################################
+
+## TODO: I would prefer this to be open-source, though I suspect
+## Excel may be easier for some users. Also save duplicate pipe-delimited file?
+
+write_xlsx(raw_data[, c(1,2,10,11,5,6,12,8,9)], 
+           path = "data/clean/Clean Costed NAPHS data.xlsx", 
+           col_names = TRUE, format_headers = FALSE)
